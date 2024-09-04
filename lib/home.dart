505d01129bcfe12d3090ddc06600a53b977dad57
Mustafakhan2 <mk9572898@gmail.com>
2024-09-04 15:17:39 +0500

    chess

---
+++ lib/home.dart
@@ -0,0 +1,520 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/rook-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/chess.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/icons8-bishop-50.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/king.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/queen-3.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/icons8-bishop-50-2.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/chess-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/rook.png',
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/pawn-2.png',
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/rook.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/chess.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/icons8-bishop-50-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/queen-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/king.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/icons8-bishop-50-2.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.black,
+                child: Image.asset(
+                  'assets/icons/chess.png',
+                  color: Colors.white,
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 40,
+                color: Colors.grey[400],
+                child: Image.asset(
+                  'assets/icons/rook.png',
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
